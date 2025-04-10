c06424155964eb21fa1f2b184c1fa81f9dd81ba8
devdiegomt <devdiegomt@gmail.com>
2025-04-09 20:16:41 -0500

    feat: pwa.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,46 @@
 import { defineConfig } from "vitest/config";
-import path from 'path';
+import path from "path";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "Tu App",
+        short_name: "App",
+        description: "Descripción de tu app",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
@@ -12,7 +48,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
